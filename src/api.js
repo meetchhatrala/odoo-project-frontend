4e907d583f9f79d92108fe797dbb55c308125093
Chhatrala Meet <meetchhatrala7@gmail.com>
2025-02-27 13:46:34 +0530

    frontend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+const API_URL = process.env.REACT_APP_BACKEND_URL 
+  ? `${process.env.REACT_APP_BACKEND_URL}/api/auth/` 
+  : "http://127.0.0.1:8000/api/auth/";
 
-const API_URL = "http://127.0.0.1:8000/api/auth/";
+// const API_URL = "http://127.0.0.1:8000/api/auth/" || ;
 
 export const signup = async (userData) => {
     return await axios.post(`${API_URL}signup/`, userData);
