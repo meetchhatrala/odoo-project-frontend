0468e24c605d0c94da5db46b16edb511780d8310
Chhatrala Meet <meetchhatrala7@gmail.com>
2025-03-02 10:49:51 +0530

    frotend

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/auth/";
+const API_URL = "http://127.0.0.1:8000/api/auth/" || "https://odoo-backend-p4vw.onrender.com";
 
 export const signup = async (userData) => {
     console.log(userData);
